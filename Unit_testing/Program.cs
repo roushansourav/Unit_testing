bf0edcf82935ca367e6b8f827abebb70c1c762df
Roushan Sourav <roushansourav@outlook.com>
2019-04-11 16:21:34 +0530

    Add project files.

---
+++ Unit_testing/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    public class Sample
+    {
+        public int Add(int x,int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x,int y)
+        {
+            return x - y;
+        }
+
+        public int GetData(int x)
+        {
+            return x;
+        }
+    }
+}
---
+++ Unit_testingTests/SampleTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unit_testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_testing.Tests
+{
+    [TestClass()]
+    public class SampleTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Sample s1 = new Sample();
+            int res = s1.Add(10, 20);
+            Assert.AreEqual(30, res);
+        }
+        [TestMethod()]
+        public void SubTest()
+        {
+            Sample s1 = new Sample();
+            int res = s1.Subtract(20, 10);
+            Assert.AreEqual(10, res);
+        }
+
+        [TestMethod()]
+        public void GetDataTest()
+        {
+            Sample s1 = new Sample();
+            int res = s1.GetData(-10);
+
+        }
+    }
+
+}
